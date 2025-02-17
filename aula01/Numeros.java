3fe7fe23b802a23fbd7af72af4b7e21494233bb4
Sara Coni <conisara96@gmail.com>
2025-02-17 09:18:47 -0300

    Aula sobre estrutura e códidos básicos em Java

---
+++ aula01/Numeros.java
@@ -0,0 +1,8 @@
+public class Numeros {
+     public static void main(String[] args) {
+      System.out.println(2 + 4);
+      System.out.println(9 - 4);
+      System.out.println(3.14 * 4);
+      System.out.println(6 / 2);
+   }
+}
