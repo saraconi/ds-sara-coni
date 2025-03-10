c3106f01d2294a52f824bf7204bca6ee7ea28f39
Sara Coni <conisara96@gmail.com>
2025-03-10 09:20:15 -0300

    Regras de tipagem e input

---
+++ aula03/TestesConversao.java
@@ -0,0 +1,17 @@
+
+   import java.util.Scanner; // import the Scanner class 
+
+class TestesConversao {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in)
+    int maxScore = 1000;
+    int userScore = 100;
+    float percentage = (float) userScore / maxScore * 100.0f;
+    System.out.println(""); 
+    userName = myObj.nextLine();   
+
+    System.out.println("User's percentage is " + percentage);]
+  }
+}
+ // TA DANDO ERRO
+ 
---
+++ aula03/TiposVariaveis.java
@@ -0,0 +1,26 @@
+public class TiposVariaveis {
+  public static void main(String[] args) {
+
+    int myNum = 5;               // integer (whole number)
+    float myFloatNum = 5.99f;    // floating point number
+    char myLetter = 'D';         // character
+    boolean myBool = true;       // boolean
+    String myText = "Hello";     // String    
+
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+
+    double MyNum = 19.99d;
+    System.out.println(MyNum);  
+
+    float f1 = 35e3f;
+    double d1 = 12E4d;
+    System.out.println(f1);
+    System.out.println(d1);
+
+       
+  }
+}
