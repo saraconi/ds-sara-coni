94eb39a0ffd52a04a3f6b648ae6c8d216bc6f538
Sara Coni <conisara96@gmail.com>
2025-04-28 09:24:16 -0300

    Aula05, usando NetBeans

---
+++ aula05/src/aula05/BalancoTrimestral.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula05;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class BalancoTrimestral {
+    public static void main(String[] args) {
+        int  gastosJaneiro = 15000;
+        int gastosFevereiro = 23000;
+        int gastosMarco = 17000;        
+        int gastosTrimestral =  gastosJaneiro + gastosFevereiro + gastosMarco ;
+        
+        System.out.println("Este é o gasto trimestral: " + gastosTrimestral);
+    }
+   
+}
